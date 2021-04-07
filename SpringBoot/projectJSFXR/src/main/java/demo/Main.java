e8cf813fb24b268246330e00db9d5317a630b586
NedianuGabirel <57480858+GabrielNedianu@users.noreply.github.com>
2021-04-07 23:02:56 +0300

    HelloWorldController

---
+++ SpringBoot/projectJSFXR/src/main/java/demo/Main.java
@@ -0,0 +1,15 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Main {
+  public static void main(String[] args) {
+    SpringApplication.run(Main.class, args);
+  }
+}
+
